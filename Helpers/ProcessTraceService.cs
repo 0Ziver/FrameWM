3f7fe6f44724f1c1998acb2def9cb0bae37bae5d
0_ziver@mail.ru <0_ziver@mail.ru>
2024-02-12 06:57:55 -0800

    Update ProcessTraceService.cs

--- Helpers/ProcessTraceService.cs
+++ Helpers/ProcessTraceService.cs
@@ -6,120 +6,56 @@ namespace Frame.Helpers
 {
     public class ProcessTraceService : IInit, IOnExit
     {
-        private const string ProcessStartTrace = "SELECT * FROM Win32_ProcessStartTrace";
-        private const string ProcessStopTrace = "SELECT * FROM Win32_ProcessStopTrace";
-
-        public static event Action<Process> OnProcessStop;
-        public static event Action<Process> OnProcessStart;
-
-
-        private ManagementEventWatcher
-            startProcessWatcher,
-            stopProcessWatcher;
-
-        private WqlEventQuery
-            startProcessQuery,
-            stopProcessQuery;
 
         public void Init()
         {
-            startProcessQuery = new(ProcessStartTrace);
-            using (startProcessWatcher = new(startProcessQuery))
-            {
-                startProcessWatcher.EventArrived += StartWatcher;
-                startProcessWatcher.Start();
-            }
-            stopProcessQuery = new(ProcessStopTrace);
-            using (stopProcessWatcher = new(stopProcessQuery))
-            {
-                stopProcessWatcher.EventArrived += StopWatcher;
-                stopProcessWatcher.Start();
-            }
-
-            /*ThreadPool.QueueUserWorkItem(_ =>
-            {
-                InstallWatcher(queryP: ProcessStartTrace, wql: startProcessQuery, watcher: startProcessWatcher);
-                InstallWatcher(queryP: ProcessStopTrace, wql: stopProcessQuery, watcher: stopProcessWatcher);
-            });*/
+        //    Watcher watcher = new Watcher(Watcher.wType.ProcessStartTrace);
         }
-
-        /*private void InstallWatcher(string queryP, WqlEventQuery wql, ManagementEventWatcher watcher)
-        {
-            wql = new(queryP);
-            using (watcher = new(wql))
-            {
-                watcher.EventArrived += ProcessWatcher;
-                watcher.Start();
-            }
-        }*/
-
-        private void UnistallWatcher(params ManagementEventWatcher[] watchers)
-        {
-            watchers[0].EventArrived -= StartWatcher;
-            watchers[1].EventArrived -= StartWatcher;
-            /*foreach (var watcher in watchers)
-            {
-                watcher.EventArrived -= ProcessWatcher;
-                watcher.Stop();
-            }*/
-        }
-
-        private int PID(EventArrivedEventArgs e)
-        {
-            return Convert.ToInt32(e.NewEvent.Properties["ProcessID"].Value);
-        }
-
-
-        void StopWatcher(object sender, EventArrivedEventArgs e)
-        {
-            string eventType = e.NewEvent.ClassPath.ClassName;
-            PropertyDataCollection properties = e.NewEvent.Properties;
-            string processName;
-            int processId;
-            processName = properties["ProcessName"].Value.ToString();
-            processId = Convert.ToInt32(properties["ProcessID"].Value);
-            Console.WriteLine($"Process close. Name: {processName}, ID: {processId}");
-        }
-
-        void StartWatcher(object sender, EventArrivedEventArgs e)
-        {
-            string eventType = e.NewEvent.ClassPath.ClassName;
-            PropertyDataCollection properties = e.NewEvent.Properties;
-            string processName;
-            int processId;
-            processName = properties["ProcessName"].Value.ToString();
-            processId = Convert.ToInt32(properties["ProcessID"].Value);
-            Console.WriteLine($"Process start. Name: {processName}, ID: {processId}");
-        }
-
-        /*private void ProcessWatcher(object sender, EventArrivedEventArgs e)
-        {
-            string eventType = e.NewEvent.ClassPath.ClassName;
-            PropertyDataCollection properties = e.NewEvent.Properties;
-            string processName;
-            int processId;
-
-            switch (eventType)
-            {
-                case "Win32_ProcessStartTrace":
-                    processName = properties["ProcessName"].Value.ToString();
-                    processId = Convert.ToInt32(properties["ProcessID"].Value);
-                    Console.WriteLine($"Process close. Name: {processName}, ID: {processId}");
-
-                    break;
-                case "Win32_ProcessStopTrace":
-                    processName = properties["ProcessName"].Value.ToString();
-                    processId = Convert.ToInt32(properties["ProcessID"].Value);
-                    Console.WriteLine($"Process close. Name: {processName}, ID: {processId}");
-
-                    break;
-            }
-        }*/
-
-
         public void OnExit()
         {
-            UnistallWatcher(startProcessWatcher, stopProcessWatcher);
         }
     }
+
+    public struct Watcher : IDisposable
+    {
+        public struct wType
+        {
+            public const string ProcessStartTrace = "SELECT * FROM Win32_ProcessStartTrace";
+            public const string ProcessStopTrace = "SELECT * FROM Win32_ProcessStopTrace";
+        }
+        public event Action<EventArrivedEventArgs> OnMessegeFromWMI;
+        private readonly ManagementEventWatcher _watcher;
+        private WqlEventQuery _eventQuery;
+
+
+        public Watcher(string query)
+        {
+            _eventQuery = new WqlEventQuery(query);
+            try
+            {
+                using (_watcher = new ManagementEventWatcher(_eventQuery))
+                {
+                    _watcher.EventArrived += Result;
+                    _watcher.Start();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void Result(object sender, EventArrivedEventArgs e)
+        {
+            OnMessegeFromWMI?.Invoke(e);
+        }
+
+        public void Dispose()
+        {
+            _watcher.EventArrived -= Result;
+            _watcher.Stop();
+            _watcher.Dispose();
+        }
+    }
+
 }
